b3531da2aa5141583498ea94803a4ad923cae8d0
shayina <nshayina@gmail.com>
2018-09-24 19:14:42 +0530

    my last commit

--- src/org/test/MyMainClass.java
+++ src/org/test/MyMainClass.java
@@ -3,6 +3,7 @@ package org.test;
 import java.util.List;
 import java.util.Map;
 
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,11 +13,11 @@ import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
+
 
 
 public class MyMainClass {
-	/*static WebDriver driver;
+	static WebDriver driver;
 	 @Given("^The user is in guru homepage$")
 	 public void the_user_is_in_guru_homepage() {
 		    System.setProperty("webdriver.chrome.driver", "C:\\Users\\SHABANA\\Desktop\\ProjectWorkspace\\JavaCucuProject\\drivers\\chromedriver.exe");
@@ -25,7 +26,7 @@ public class MyMainClass {
 		}
 	 
 	@When("^The user enters userId and password$")
-	public void the_user_enters_userId_and_password(DataTable Credentials) {*/
+	public void the_user_enters_userId_and_password(DataTable Credentials) {
 		//List<String> myList=Credentials.asList(String.class);
 		//driver.findElement(By.name("uid")).sendKeys(myList.get(0));
 		//driver.findElement(By.name("password")).sendKeys(myList.get(1));
@@ -38,9 +39,10 @@ public class MyMainClass {
 	//driver.findElement(By.name("uid")).sendKeys(myList.get(3).get(0));
 	//driver.findElement(By.name("password")).sendKeys(myList.get(3).get(1));
 	
-	/*List<Map<String,String>> myMap=Credentials.asMaps(String.class, String.class);
+	List<Map<String,String>> myMap=Credentials.asMaps(String.class, String.class);
 	driver.findElement(By.name("uid")).sendKeys(myMap.get(2).get("username"));
 	driver.findElement(By.name("password")).sendKeys(myMap.get(2).get("password"));
+	
 	
 	}
 	
@@ -52,7 +54,9 @@ public class MyMainClass {
 	@Then("^The user should see the fields empty$")
 	public void the_user_should_see_the_fields_empty() {
 		System.out.println("The fields are empty");
-		//Assert.assertEquals("driver.findElement(By.name("password")).getAttribute("value")","password");
+		
+		String output=driver.findElement(By.name("password")).getAttribute("value");
+		Assert.assertEquals(output,"");
 		
 	}
-*/}
+}
